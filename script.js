6b7dda10352f2c9c43efa76b648e0234aef40cf9
James Alderman <80925002+JAldo1980@users.noreply.github.com>
2022-06-08 11:02:27 +0100

    JavaScript functionality
    Added the ability to 'click' on the email button which enabled copy & paste function.

---
+++ script.js
@@ -0,0 +1,19 @@
+const email = document.getElementById("email");
+const btn = document.getElementById("btn");
+
+// now I need to hide the input field...
+document.getElementById("email").style.position = "absolute";
+email.style.top = '-9999px';
+email.style.left = '-9999px';
+
+
+btn.onclick = function () {
+    // step 1 - select text
+    email.select();
+
+    // step 2 - copy the text
+    document.execCommand("copy");
+}
+
+
+
